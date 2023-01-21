80dc9b72a7204abdd716ab1487224cd270b1d07a
Jeff Zarnett <jzarnett@gmail.com>
2023-01-21 14:37:30 -0500

    Initial version of Rust repo creator

---
+++ README.md
@@ -0,0 +1,60 @@
+# Project Repo Creator
+
+Here's a small tool that I wrote to help me set up repos in the UW gitlab in an efficient way. 
+
+Expectations: 
+
+1. You have created a gitlab group (e.g., `ece459-1231`) that will contain all the repos. You can add other instructors, lab instructors, and marking TAs as members of that group so you don't have to add them to each repo and you'll have access as needed.
+
+2. You have a template repo with the starter code (or at least starter repo configuration) that you want to distribute to everyone. A basically-empty repo is fine, just throw a README in there if you must.
+
+3. Your file of students and groups to create is prepared (correctly); see the "Usage" section for what to do.
+
+Output:
+
+The tool creates git repositories in the designated gitlab group that contain the starter code pointed to by the template repository.
+
+The group members or student specified in the file are (is) added as direct member(s) of the project with Developer permissions -- it allows them to do work but not to mess with the setup in a way that would be bad for marking.
+
+The repos are created with branch protections enabled that prevent force pushes. This is important, because without this, a student could have a commit before a deadline, do more work after the deadline, and rewrite history to make it appear that the work was done before the deadline in the commit history.
+
+## Usage
+I tried to make it easy but there are a few things that could not be avoided. It takes five commandline arguments (order and format matter, sadly).
+
+Formally:
+```
+executable <designation> <gitlab_group_name> <template_repo> <list_of_students_or_groups.csv> <token_file>
+```
+
+In order then:
+### `designation`
+The designation refers to how this repo should be designated: typically assignment 1 would be given as `a1`, but you could say it's the final exam by putting `final`, or a project by `p`. Your call.
+
+### `gitlab_group_name`
+This is the group in gitlab where you want the repos to be created. This parameter is also used in building the name of the repo (see below). It's not so much that it is important for the student -- they probably only take the course once -- but it really helps course staff if names are clear even without looking at the group the project is in. So if the course and term I'm running this in are ECE 459 and 1231 (Winter 2023), I would choose `ece459-1231`.
+
+### `template_repo`
+This is where the starter code is found for the repo. The repo obviously must exist and it's formed as `group/repo` like `ece459-starter-code/ece459-a1`. The repo there is used as the basis and it includes files as well as authors/commit history, so be mindful that if you had a (partial) solution in there at some point and deleted things to make it the starter code, that history is visible.
+
+### `list_of_students_or_groups.csv`
+Provide the filename of a CSV (comma-separated-value) format file that contains information about the students and groups to be created. The only content here is student usernames (e.g., jzarnett for me). You have two options about what to do here (and can mix and match):
+
+If there is EXACTLY one username on a line, the repo will be created as `group-desigation-username`, so `ece459-1231-a1-jzarnett`. That username will be added as a member of the project.
+
+If there are MULTIPLE usernames on a line, the repo will be created as `group-designation-g{linenumber+1}`. So if this is the 8th line of the CSV file it will be created as `ece459-1231-proj-g9`. All usernames on that line will be added as members of the project. 
+
+### `token_file`
+A plain text file containing your gitlab user token. Whoever the token belongs to is the owner of the repos that we are creating here. No newline or anything at the end of the file.
+
+
+## TODOs
+- Right now I have a `sleep` call in between creating the repo and trying to set the branch protection rules. That's annoying, but I wasn't easily able to solve it in the first version.
+- This isn't parallelized, though in practice I'd like to try doing 2-3 repos at once. Helps when there's 400+ students. 
+- At some point the default branch should change from `master` to `main` and it would be nice if the program could handle that automatically
+- Groups are always assigned sequential numbers based on their order in the file. In the future, maybe support a prefix so the group number could be something else, like "201.1" if the groups are subdivided in some way (e.g., by lab section).
+- Letting you give params in any order might be good.
+
+## Changelog
+
+### 1.0.0
+Initial, non-parallelized version with ugly sleep call but it does work.
---
+++ src/main.rs
@@ -0,0 +1,221 @@
+use std::{env, fs};
+use std::thread::sleep;
+use std::time::Duration;
+
+use csv::ReaderBuilder;
+use gitlab::api::common::{AccessLevel, ProtectedAccessLevel, VisibilityLevel};
+use gitlab::api::groups::GroupBuilder;
+use gitlab::api::projects::members::AddProjectMemberBuilder;
+use gitlab::api::projects::protected_branches::ProtectedAccess;
+use gitlab::api::users::{CurrentUserBuilder, UsersBuilder};
+use gitlab::api::{projects, Query};
+use gitlab::Gitlab;
+
+use serde::Deserialize;
+
+const UW_GITLAB_URL: &str = "git.uwaterloo.ca";
+// One day this will be "main" but for now...
+const DEFAULT_BRANCH_NAME: &str = "master";
+
+#[derive(Debug, Deserialize)]
+struct Project {
+    id: u64,
+}
+
+#[derive(Debug, Deserialize)]
+struct ProjectUser {
+    id: u64,
+}
+
+#[derive(Debug, Deserialize)]
+struct CurrentUser {
+    username: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct ProjectGroup {
+    id: u64,
+}
+
+struct GitLabConfig {
+    designation: String,
+    group_name: String,
+    template_repo: String,
+    token: String,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 6 {
+        println!(
+            "Usage: {} <designation> <gitlab_group_name> <template_repo> <list_of_student_groups.csv> <token_file>",
+            args.get(0).unwrap()
+        );
+        println!(
+            "Example: {} a1 ece459-1231 ece459/ece459-a1 students.csv token.git",
+            args.get(0).unwrap()
+        );
+        return;
+    }
+    let token = read_token_file(args.get(5).unwrap());
+    let config = GitLabConfig {
+        designation: String::from(args.get(1).unwrap()),
+        group_name: String::from(args.get(2).unwrap()),
+        template_repo: String::from(args.get(3).unwrap()),
+        token: token.clone()
+    };
+
+    let repo_members = parse_csv_file(args.get(4).unwrap());
+    let client = Gitlab::new(String::from(UW_GITLAB_URL), token).unwrap();
+
+    create_repos(client, repo_members, config)
+}
+
+fn create_repos(client: Gitlab, repo_members: Vec<Vec<String>>, config: GitLabConfig) {
+    let current_user = find_current_user(&client);
+    let import_url = format! {
+        "https://{}:{}@git.uwaterloo.ca/{}.git", current_user, config.token, config.template_repo
+    };
+    let project_group_id = find_group_by_name(config.group_name.clone(), &client);
+
+    for i in 0..repo_members.len() {
+        let group_or_student = repo_members.get(i).unwrap();
+
+        let project_name = if group_or_student.len() == 1 {
+            format!(
+                "{}-{}-{}",
+                config.group_name,
+                config.designation,
+                group_or_student.get(0).unwrap()
+            )
+        } else {
+            format!("{}-{}-g{}", config.group_name, config.designation, (i + 1))
+        };
+
+        let project = create_project(
+            &client,
+            import_url.clone(),
+            project_name.clone(),
+            project_group_id,
+        );
+        println!("Created project {} with id {}!", project_name, project.id);
+        // Sleep seems to be needed here otherwise the branch protection call gets a 404
+        // I presume it's because gitlab is setting up stuff in the background but argh
+        sleep(Duration::new(10, 0));
+
+        configure_branch_protection(&client, &project_name, project.id);
+
+        add_users_to_project(&client, group_or_student, &project_name, project.id);
+
+        println! {"Setup of repo {} is complete.", project_name}
+    }
+}
+
+fn create_project(
+    client: &Gitlab,
+    import_url: String,
+    project_name: String,
+    project_group_id: u64,
+) -> Project {
+    println!("Creating project {}...", project_name);
+    let project_builder = projects::CreateProjectBuilder::default()
+        .namespace_id(project_group_id)
+        .name(&project_name)
+        .visibility(VisibilityLevel::Private)
+        .default_branch(DEFAULT_BRANCH_NAME)
+        .import_url(import_url)
+        .build()
+        .unwrap();
+
+    let project: Project = project_builder.query(client).unwrap();
+    project
+}
+
+fn add_users_to_project(
+    client: &Gitlab,
+    group_or_student: &Vec<String>,
+    project_name: &String,
+    project_id: u64,
+) {
+    for student in group_or_student {
+        println!("Adding student {} to project {}...", student, project_name);
+        let gl_user_builder = UsersBuilder::default().search(student).build().unwrap();
+        let gl_user: Vec<ProjectUser> = gl_user_builder.query(client).unwrap();
+        let gl_user = gl_user.get(0).unwrap();
+
+        let member_builder = AddProjectMemberBuilder::default()
+            .project(project_id)
+            .access_level(AccessLevel::Developer)
+            .user(gl_user.id)
+            .build()
+            .unwrap();
+
+        // This request also returns a map but I don't really care... Danke, ich hasse es
+        member_builder.query(client).unwrap_or(());
+    }
+}
+
+fn configure_branch_protection(client: &Gitlab, project_name: &String, project_id: u64) {
+    println!("Protecting default branch in project {}...", project_name);
+    // First, unprotect the default branch so that we are sure we set the right values
+    let unprotect_branch_builder = projects::protected_branches::UnprotectBranchBuilder::default()
+        .project(project_id)
+        .name(DEFAULT_BRANCH_NAME)
+        .build()
+        .unwrap();
+
+    // The gitlab library somehow thinks HTTP 204 is an error -_-
+    unprotect_branch_builder.query(client).unwrap_or(());
+
+    let protect_branch_builder = projects::protected_branches::ProtectBranchBuilder::default()
+        .project(project_id)
+        .name(DEFAULT_BRANCH_NAME)
+        .allow_force_push(false)
+        .allowed_to_merge(ProtectedAccess::Level(ProtectedAccessLevel::Developer))
+        .allowed_to_push(ProtectedAccess::Level(ProtectedAccessLevel::Developer))
+        .allowed_to_unprotect(ProtectedAccess::Level(ProtectedAccessLevel::Admin))
+        .build()
+        .unwrap();
+
+    // This request returns a map but I don't really care... Danke, ich hasse es
+    protect_branch_builder.query(client).unwrap_or(());
+}
+
+fn find_current_user(client: &Gitlab) -> String {
+    println!("Finding current user...");
+    let current_user_builder = CurrentUserBuilder::default().build().unwrap();
+    let curr_user: CurrentUser = current_user_builder.query(client).unwrap();
+    curr_user.username
+}
+
+fn find_group_by_name(group_name: String, client: &Gitlab) -> u64 {
+    println!("Finding group ID for group {}...", group_name);
+    let group_builder = GroupBuilder::default().group(group_name).build().unwrap();
+
+    let proj_group: ProjectGroup = group_builder.query(client).unwrap();
+    proj_group.id
+}
+
+fn parse_csv_file(filename: &String) -> Vec<Vec<String>> {
+    let mut result: Vec<Vec<String>> = Vec::new();
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(filename)
+        .unwrap();
+
+    for line in rdr.records() {
+        let line = line.unwrap();
+        let mut inner = Vec::new();
+        for user in line.iter() {
+            inner.push(String::from(user))
+        }
+        result.push(inner);
+    }
+    result
+}
+
+fn read_token_file(filename: &String) -> String {
+    let token = fs::read_to_string(filename)
+        .expect(&format!{"Unable to read token from file {}", filename});
+    token
+}
